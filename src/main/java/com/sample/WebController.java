6650db827d1f6720aa4a03340d1331223be3d71a
sourabhyadav01 <sourabhyadav01@sourabh2761.Nagarro.local>
2019-08-06 16:56:10 +0530

    Updated controller

--- src/main/java/com/sample/WebController.java
+++ src/main/java/com/sample/WebController.java
@@ -1,18 +1,19 @@
 package com.sample;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/compliance")
+@RequestMapping("/start")
 public class WebController {
 	
-	@GetMapping
-    public void start()
+	@GetMapping("/{name}")	
+    public String start(@PathVariable(value="name") String name)
     {
-       System.out.println("Running data compliance service...........");
-       //TODO : Call RideData compliance service for cleanup 
+       System.out.println("Running Web API service...........");
+       return "Web App " + name;
     }
 
 }
